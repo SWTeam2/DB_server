0837c015f1ebc0fdedfcff31f5c16ade9abec076
KimJeongHo <76683835+Jeongho-K@users.noreply.github.com>
2023-08-11 16:22:47 +0900

    edit post to get

--- server/app/app_routing.py
+++ server/app/app_routing.py
@@ -1,32 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-
 app = FastAPI()
 
-
-# Sample function that takes parameters and returns data
-def process_data(bearing: str, csv_num: int):
-    # Replace this with your actual data processing logic
-    data = {
-        "bearing": bearing,
-        "start_line": csv_num,
-        "result": f"Processed data for bearing {bearing} between lines {csv_num}"
-    }
-    return data
-
-
-# Define a Pydantic model for the request payload
-class RequestPayload(BaseModel):
+class RequestParams(BaseModel):
     bearing: str
     csv_num: int
 
-
-@app.post("/process/")
-async def process_request(payload: RequestPayload):
+@app.get("/process/")
+async def process_request(params: RequestParams):
     try:
-        result = process_data(
-            payload.bearing, payload.csv_num)
+        result = {
+            "bearing": params.bearing,
+            "start_line": params.csv_num,
+            "result": f"Processed data for bearing {params.bearing} between lines {params.csv_num}"
+        }
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
