bc1ac572e4b2ce6de525a9f5d56eba2a111c5337
kimdaeun <onlydk57@gmail.com>
2023-08-17 20:22:33 +0900

    raw 데이터 api 수정

--- server/app/app_routing.py
+++ server/app/app_routing.py
@@ -1,40 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from app.outputdb import contact_raw_id
-from app.outputdb import contact_raw
-from app.output_pred import contact_pred
 
 app = FastAPI()
 
 
-@app.get("/data/")
-async def get_data_request(table: str=None, id: str=None, csv_num: str=None):
+@app.get("/data/{table}/{id}")
+async def get_data_request(table: str=None, id: int=None):
     try:
-        if id == None:
-            result = contact_raw_id(table, id)
-
-        if csv_num == None:
-            result = contact_raw(table, csv_num)
-
-        else:
-            result = contact_pred(table, id)
+        result = contact_raw_id(table, id)
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-@app.post("/infer/")
-async def post_prediction_request(table: str=None, pred_id: str=None):
-    try:
-        pass
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-# @app.get("/data/")
-# async def get_data_request(table: str=None, csv_num: str=None, pred_id: str=None):
-#     try:
-#         if pred_id == None:
-#             result = contact_raw_id(table, csv_num)
-#         else:
-#             result = contact_pred(table, pred_id)
-#         return result
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
--- server/app/outputdb.py
+++ server/app/outputdb.py
@@ -29,30 +29,6 @@ def create_table_if_not_exists(conn, name):
         print(f"Table {name} does not exists.")
     cur.close()
 
-
-def contact_raw(name, csv_num):
-    conn = connect_db(name)
-    cur = conn.cursor()
-    # query = f"SELECT hour, minutes, second, microsecond, horiz_accel, vert_accel, csv_number FROM {name} WHERE csv_number = '{csv_num}'"
-    query = f"SELECT * FROM {name} WHERE csv_number = '{csv_num}'"
-    # SQL 쿼리 실행
-    cur.execute(query)
-
-    # 결과 가져오기
-    rows = cur.fetchall()
-    column_names = [desc[0] for desc in cur.description]
-
-    # 결과를 딕셔너리 형태로 변환
-    data_as_dict = [dict(zip(column_names, row)) for row in rows]
-
-    # 딕셔너리를 JSON 형태로 직렬화
-    json_data = json.dumps(data_as_dict, indent=4)
-
-    # 커서 및 연결 종료
-    cur.close()
-
-    return json_data
-
 # data =  contact_data('learning_table16', 'acc_00001')
 
 def contact_raw_id(table, id):
@@ -78,51 +54,3 @@ def contact_raw_id(table, id):
     cur.close()
 
     return json_data
-
-def contact_raw(name, csv_num):
-    conn = connect_db(name)
-    cur = conn.cursor()
-    # query = f"SELECT hour, minutes, second, microsecond, horiz_accel, vert_accel, csv_number FROM {name} WHERE csv_number = '{csv_num}'"
-    query = f"SELECT * FROM {name} WHERE csv_number = '{csv_num}'"
-    # SQL 쿼리 실행
-    cur.execute(query)
-
-    # 결과 가져오기
-    rows = cur.fetchall()
-    column_names = [desc[0] for desc in cur.description]
-
-    # 결과를 딕셔너리 형태로 변환
-    data_as_dict = [dict(zip(column_names, row)) for row in rows]
-
-    # 딕셔너리를 JSON 형태로 직렬화
-    json_data = json.dumps(data_as_dict, indent=4)
-
-    # 커서 및 연결 종료
-    cur.close()
-
-    return json_data
-
-# data =  contact_data('learning_table16', 'acc_00001')
-
-# def contact_raw_id(table, id):
-#     conn = connect_db(table)
-#     cur = conn.cursor()
-#     query = f"SELECT * FROM {table} WHERE csv_number IN (SELECT csv_number FROM {table} WHERE id = {id})"
-#     # SQL 쿼리 실행
-#     cur.execute(query)
-
-#     # 결과 가져오기
-#     rows = cur.fetchall()
-#     column_names = [desc[0] for desc in cur.description]
-
-#     # 결과를 딕셔너리 형태로 변환
-#     data_as_dict = [dict(zip(column_names, row)) for row in rows]
-
-#     # 딕셔너리를 JSON 형태로 직렬화
-#     json_data = json.dumps(data_as_dict, indent=4)
-
-#     # 커서 및 연결 종료
-#     cur.close()
-
-#     return json_data
-
