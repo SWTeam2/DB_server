b49277b73a62fb89af8bd7e67599d2b7a8870d46
choi4624 <choi4624@gmail.com>
2023-08-21 20:25:28 +0900

    commit image

--- util/test_table.py
+++ util/test_table.py
@@ -17,7 +17,7 @@ conn = psycopg2.connect(
 )
 
 # CSV 파일 디렉토리 설정
-csv_directory = '/home/dbuser-4624/dataBase/ieee-phm-2012-data-challenge-dataset-master/Test_set/Bearing3_3'
+csv_directory = '/home/dbuser-4624/dataBase/ieee-phm-2012-data-challenge-dataset-master/Full_Test_Set/Bearing1_6'
 
 # 열 이름 설정
 column_names = ['hour', 'minutes', 'second', 'microsecond', 'horiz_accel', 'vert_accel']
@@ -26,7 +26,7 @@ column_names = ['hour', 'minutes', 'second', 'microsecond', 'horiz_accel', 'vert
 cur = conn.cursor()
 
 # 테이블 생성
-table_name = 'Test_table_bearing3_3'
+table_name = 'Full_Test_table_bearing1_6'
 
 # 테이블 생성 구문
 create_table_sql = f'''
@@ -49,33 +49,38 @@ conn.commit()
 # CSV 파일 읽기 및 데이터 삽입
 csv_files = [f for f in os.listdir(csv_directory) if f.endswith('.csv')]
 
-print(csv_files)
-
 # 파일명 순서대로 정렬
 csv_files.sort()
 
-print(csv_files)
-
-for csv_file in csv_files:
+for file_num, csv_file in enumerate(csv_files, start=1):
     csv_file_name = os.path.splitext(csv_file)[0]  # 파일명에서 .csv 제외
     csv_file_path = os.path.join(csv_directory, csv_file)
 
     with open(csv_file_path, 'r') as file:
         reader = csv.reader(file)
         next(reader)  # 첫 번째 행은 건너뛰기 (컬럼 이름)
+
+        # Batch inserts within a transaction
+        rows_to_insert = []
         for idx, row in enumerate(reader, start=1):
             if len(row) == len(column_names):  # 데이터의 열 수가 일치하는 경우
                 row.append(csv_file_name)  # csv_number 열에 파일명 추가
-
-                # 마이크로초 데이터 변환
                 row[3] = int(float(row[3]))  # '1.0004e+005'를 정수로 변환
-            
-                insert_sql = f'''
-                    INSERT INTO {table_name} (hour, minutes, second, microsecond, horiz_accel, vert_accel, csv_number) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s);
-                '''
-                cur.execute(insert_sql, row)
-                print(insert_sql)
+                rows_to_insert.append(tuple(row))
+
+        insert_sql = f'''
+            INSERT INTO {table_name} (hour, minutes, second, microsecond, horiz_accel, vert_accel, csv_number) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s);
+        '''
+        cur.executemany(insert_sql, rows_to_insert)
+
+    # Commit the transaction after each file is processed
+    conn.commit()
+
+    # Print progress
+    print(f"Processed file {file_num} of {len(csv_files)}: {csv_file}")
+    
+# Commit the transaction after all files are processed
 conn.commit()
 
 # 연결 종료
